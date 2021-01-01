2878e5d51a56e25e5c05edeaeb7d2e616997f46b
= <=>
2021-01-01 21:04:24 +0700

    Map

---
+++ app/src/main/java/com/example/personal_note/fragment/EditProfile.java
@@ -0,0 +1,16 @@
+package com.example.personal_note.fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.personal_note.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
