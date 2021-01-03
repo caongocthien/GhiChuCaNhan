9b6d6d20c5c524390ef3d5a499cf452bec5c93c7
trandinhthang <thang.tran.5243817@gmail.com>
2021-01-03 14:12:04 +0700

    TDT_NavigationDrawer

--- app/src/main/java/com/example/personal_note/HomeActivity.java
+++ app/src/main/java/com/example/personal_note/HomeActivity.java
@@ -1,9 +1,13 @@
 package com.example.personal_note;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.PieData;
@@ -14,13 +18,11 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        PieChart pieChart =findViewById(R.id.piechart);
+public class HomeActivity extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_home, container, false);
+        PieChart pieChart = (PieChart) view.findViewById(R.id.pieChart);
         pieChart.setUsePercentValues(true);
 
         Description desc = new Description();
@@ -33,9 +35,9 @@ public class HomeActivity extends AppCompatActivity {
         pieChart.setTransparentCircleRadius(0f);
 
         List<PieEntry> value = new ArrayList<>();
-        value.add(new PieEntry( 60f,"Processing"));
-        value.add(new PieEntry(20f,"Pending"));
-        value.add(new PieEntry(20f,"Done"));
+        value.add(new PieEntry(60f, "Processing"));
+        value.add(new PieEntry(20f, "Pending"));
+        value.add(new PieEntry(20f, "Done"));
 
         PieDataSet pieDataSet = new PieDataSet(value, "TYPE");
 
@@ -44,6 +46,11 @@ public class HomeActivity extends AppCompatActivity {
         pieChart.setData(pieData);
         pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
 
-        pieChart.animateXY(1400,1400);
+        pieChart.animateXY(1400, 1400);
+        return view;
     }
+
 }
+
+
+
