09f6426d7c6c16c935603e4f94ef42d99c16658b
caongocthien <caongocthien36@gmail.com>
2021-01-03 15:24:27 +0700

    add dialog

--- app/src/main/java/com/example/personal_note/NoteActivity.java
+++ app/src/main/java/com/example/personal_note/NoteActivity.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.personal_note.ui.home.HomeViewModel;
 
 public class NoteActivity extends Fragment {
 
