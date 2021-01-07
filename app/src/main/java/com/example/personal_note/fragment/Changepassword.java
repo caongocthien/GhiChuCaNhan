06674e487e9badd6d562ec2e985790c93362f3a4
caongocthien <caongocthien36@gmail.com>
2021-01-07 13:37:12 +0700

    btnHome Changepassword

--- app/src/main/java/com/example/personal_note/fragment/Changepassword.java
+++ app/src/main/java/com/example/personal_note/fragment/Changepassword.java
@@ -1,14 +1,19 @@
 package com.example.personal_note.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.example.personal_note.HomeActivity;
+import com.example.personal_note.NavigationActivity;
 import com.example.personal_note.R;
 
 public class Changepassword extends Fragment {
@@ -18,5 +23,25 @@ public class Changepassword extends Fragment {
 
         View root = inflater.inflate(R.layout.activity_changepassword, container, false);
         return root;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnHome =view.findViewById(R.id.bntHome);
+            btnHome.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), NavigationActivity.class);
+                    startActivity(intent);
+
+
+
+
+
+                }
+            });
+
     }
 }
