656e5c35b4b20aa3f17037fb7ac1e84d2b6da497
vietthang1107 <nvt1999@gmail.com>
2021-01-10 15:25:52 +0700

    NoteAdapter_UI

--- app/src/main/java/com/example/personal_note/adapter/NoteAdapter.java
+++ app/src/main/java/com/example/personal_note/adapter/NoteAdapter.java
@@ -1,14 +1,43 @@
 package com.example.personal_note.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.personal_note.R;
+import com.example.personal_note.db.Note;
+
+
 
 import java.util.List;
 
-public class NoteAdapter extends ArrayAdapter {
+
+public class NoteAdapter extends ArrayAdapter<Note> {
     public NoteAdapter(@NonNull Context context, int resource, @NonNull List objects) {
-        super(context, resource, objects);
+        super(context, 0, objects);
+    }
+    @NonNull
+
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Note note = getItem(position);
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.status_layout,parent,false);
+            TextView tvNoteCate = convertView.findViewById(R.id.tvNoteCategory);
+            TextView tvNoteDate = convertView.findViewById(R.id.tvNotePlanDate);
+            TextView tvNotePri = convertView.findViewById(R.id.tvNotePriority);
+
+            tvNoteCate.setText(note.getIdCategory());
+            tvNoteDate.setText(note.getDate());
+            tvNotePri.setText(note.getIdPriority());
+        }
+        return convertView;
     }
 }
