cc9b53e0c67390e3768666abe00b39f400981f7d
trandinhthang <thang.tran.5243817@gmail.com>
2021-01-14 18:48:11 +0700

    trong

--- app/src/main/java/com/example/personal_note/SplashScreenActivity.java
+++ app/src/main/java/com/example/personal_note/SplashScreenActivity.java
@@ -1,6 +1,8 @@
 package com.example.personal_note;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,8 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toolbar;
+
+import com.example.personal_note.fragment.AddNoteFragment;
 
 public class SplashScreenActivity extends AppCompatActivity {
     ImageView myimg;
@@ -22,15 +26,21 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         myimg = (ImageView) findViewById(R.id.noteImg);
-//        mytv = (TextView) findViewById(R.id.txtSplash);
         myanim = AnimationUtils.loadAnimation(this, R.anim.myanim);
         myimg.startAnimation(myanim);
-        mytv.startAnimation(myanim);
-        homeAct = new Intent(this, SignUpActivity.class);
+//        mytv.startAnimation(myanim);
+
+
+//        homeAct = new Intent(this, manager.getClass());
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(homeAct);
+                AddNoteFragment fragment = new AddNoteFragment();
+                FragmentManager manager = getSupportFragmentManager();
+                manager.beginTransaction().add(R.id.notefrag,fragment).commit();
+
+
+//                startActivity(homeAct);
                 finish();
             }
         }, 3000);
